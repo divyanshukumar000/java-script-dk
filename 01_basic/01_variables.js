b619b934155a25089aa9f3f3c3e7ffcb2816d150
Divyanshu Kumar <divyanshuseth071@gmail.com>
2025-05-20 15:16:19 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 62077
+let accountEmail = "divyanshuseth071@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bihar"
+
+let accountState;
+// accountId = 45 not allowed
+
+accountEmail = "komal123@gmail.com"
+accountPassword = "54321"
+accountCity = "Patna"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+console.log(3+3);
+// code readability should be high; its wrong way to write code
+console.log("Divyanshu")
+
+let name = "Divyanshu"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof 5); // example- 5 is number
+console.log(typeof null); // example- null is object
+
