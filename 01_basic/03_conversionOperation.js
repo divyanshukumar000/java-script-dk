f31427cbf6451e5bba6afa707965e2e4bd7494ab
Divyanshu Kumar <divyanshuseth071@gmail.com>
2025-05-21 14:56:08 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// "45" => 45
+// 45abc => NaN (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedIn = "Divyanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0=> false
+// "" => false
+// "Divyanshu" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
