99733ffc789d1fd8036f9097b3e1aeec320e18f1
Divyanshu Kumar <divyanshuseth071@gmail.com>
2025-05-22 14:10:46 +0000

    comparision

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Divyanshu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // 1 => true; 0=> false
@@ -27,5 +27,43 @@ console.log(booleanIsLoggedIn)
 let someNumber = 45
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+//******************* Operations **********************//
+
+let value = 5
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(8-4)
+// console.log(4*4)
+
+let str1 = "Komal"
+let str2 = "Divyanshu"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log("2"+1);
+console.log("1"+(2+2));
+console.log(1+2+"3");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
