41d5d76b038d5df033187c83446ff56e12733f3a
Divyanshu Kumar <divyanshuseth071@gmail.com>
2025-05-22 14:13:15 +0000

    comparision of data

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,40 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// all are simple check true or false
+
+
+// console.log("2" > 1); 
+// console.log("02" > 1);
+//check string and number
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//we avoid this type of conversion
+
+// console.log(undefined ==0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === 
+
+console.log("2" === 2);  //for strictly check use ===
+
+
+
+
+
+
+
+
+
+
+
+
+
